a81188a22c3558d03b957d51bd8bc56bc71aed33
angusthefilthyteemomain.github.io <angusozzie@gmail.com>
2019-07-03 09:02:36 -0700

    a lot of dif things

--- Demo Day Project/Assets/Script/ChangeRoomScript.cs
+++ Demo Day Project/Assets/Script/ChangeRoomScript.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeRoomScript : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraScript cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +31,18 @@ public class ChangeRoomScript : MonoBehaviour
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
         }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(3f);
+        text.SetActive(false);
     }
 }
