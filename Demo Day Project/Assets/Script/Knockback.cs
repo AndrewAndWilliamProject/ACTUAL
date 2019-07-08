bec6147d3cc992d9d96bbf1342121b7297c4afbc
angusthefilthyteemomain.github.io <angusozzie@gmail.com>
2019-07-08 09:02:20 -0700

    fixed world enemy e=follow

--- Demo Day Project/Assets/Script/Knockback.cs
+++ Demo Day Project/Assets/Script/Knockback.cs
@@ -15,7 +15,7 @@ public class Knockback : MonoBehaviour
             Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
             if(enemy != null)
             {
-                enemy.isKinematic = false;
+                enemy.GetComponent<Enemy>().currentState = EnemyState.stagger;
                 Vector2 difference = enemy.transform.position - transform.position;
                 difference = difference.normalized * thurst;
                 enemy.AddForce(difference, ForceMode2D.Impulse);
@@ -30,7 +30,7 @@ public class Knockback : MonoBehaviour
         {
             yield return new WaitForSeconds(knockTime);
             enemy.velocity = Vector2.zero;
-            enemy.isKinematic = true;
+            enemy.GetComponent<Enemy>().currentState = EnemyState.idle;
         }
     }
 }
