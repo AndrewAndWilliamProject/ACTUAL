6b3191f7482e2d42fe02c144d206da80e06c9cc2
angusthefilthyteemomain.github.io <angusozzie@gmail.com>
2019-07-11 21:05:15 -0700

    a lot of changes

---
+++ Array Project/Assets/script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(add());
+        Debug.Log(subtract());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int add()
+    {
+        int result = 6 + 6;
+        return result;
+    }
+
+    int subtract()
+    {
+        int result = 9 - 6;
+        return result;
+    }
+}
