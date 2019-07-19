516e7117bceadcc5f960e48645c8006e38548351
angusthefilthyteemomain.github.io <angusozzie@gmail.com>
2019-07-18 22:16:45 -0700

    I have made a ton of updates and am gonna push so Nicky you can see the dif

---
+++ Demo Day Project/Assets/Script/SceneTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+
+    public string GoToScene;
+    private float xpos;
+    private float ypos;
+
+    void Start()
+    {
+        xpos = -11.5f;
+        ypos = 0f;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+
+    {
+        if (collider.CompareTag("Player") && !collider.isTrigger)
+        {
+            PlayerPrefs.SetFloat("xpos", xpos);
+            PlayerPrefs.SetFloat("ypos", ypos);
+            SceneManager.LoadScene(GoToScene);
+        }
+    }
+}
